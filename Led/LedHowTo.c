24421a7f2c4e3f2542349267dc29a18f7f156fe7
jaejunlee <jaejun0201@gmail.com>
2015-02-24 20:56:36 +0900

    First Commit

---
+++ Led/LedHowTo.c
@@ -0,0 +1,22 @@
+task main()
+{
+/*
+  LED_BLACK                     = 0,
+  LED_GREEN                     = 1,
+  LED_RED                       = 2,
+  LED_ORANGE                    = 3,
+  LED_GREEN_FLASH               = 4,
+  LED_RED_FLASH                 = 5,
+  LED_ORANGE_FLASH              = 6,
+  LED_GREEN_PULSE               = 7,
+  LED_RED_PULSE                 = 8,
+  LED_ORANGE_PULSE              = 9,
+  */
+
+  for(int i=0;i<LEDPATTERNS;i++)
+  {
+  	setLEDColor(i);
+  	wait1Msec(1000);
+  }
+
+}
